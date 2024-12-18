c8dfa3796ef7577e0c31b902fa195e0da6d1fd27
Subash A <subash.annadurai@shopup.org>
2024-12-18 23:09:47 +0530

    Deploy Tailwind app to GitHub Pages

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+    content: ["./index.html"],
+    theme: {
+      extend: {
+        colors: {
+          black: "#151515",
+          white: "#ffffff",
+          green: "#12B76A",
+          purple: {
+            700: "#420466",
+            900: "#3a0055",
+            500: "#6E0DA5"
+          },
+        },
+        fontFamily: {
+          customFont1: ['"abril"', 'sans-serif'],
+          customFont2: ['"cormorant"', 'serif'],
+          customFont3: ['"inter"', 'monospace'],
+        },
+      },
+    },
+    plugins: [],
+  };
+  
